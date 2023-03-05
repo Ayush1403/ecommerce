d09acd1c9ec380414c9bf930bb790ee8274cfb13
Mayank0404 <mayanksharma040404@gmail.com>
2023-03-05 22:30:24 +0530

    login and register done in firebase

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -1,18 +1,74 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup extends AppCompatActivity {
+    Button btn_signup,btn_loginPage;
+    TextInputEditText signup_email,signup_phone,signup_name,signup_password;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        db=FirebaseFirestore.getInstance();
+        signup_email=findViewById(R.id.signup_email);
+        signup_phone=findViewById(R.id.signup_phone);
+        signup_name=findViewById(R.id.signup_name);
+        signup_password=findViewById(R.id.signup_password);
+        btn_signup=findViewById(R.id.signup_btn);
+        btn_loginPage=findViewById(R.id.signup_login_page);
+        btn_loginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Signup.this,login_page.class);
+                startActivity(intent);
+            }
+        });
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> user = new HashMap<>();
+                user.put("email", signup_email.getText().toString());
+                user.put("name", signup_name.getText().toString());
 
+                user.put("phone", signup_phone.getText().toString());
+                user.put("password", signup_password.getText().toString());
+
+
+
+
+                db.collection("students").document(signup_email.getText().toString()).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(Signup.this, "USER REGISTERED", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Signup.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/login_page.java
+++ app/src/main/java/com/example/myapplication/login_page.java
@@ -1,44 +1,82 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class login_page extends AppCompatActivity {
+       Button btn_login,btn_signup;
+       TextInputEditText username,password;
+       FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_login_page);
-        Button btn=findViewById(R.id.sgp);
-        Button btn2 = findViewById(R.id.sgi);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity();
-            }
-        });
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity2();
-            }
-        });
-    }
-    public void openActivity(){
-        Intent intent = new Intent(this,MainScreen.class);
-        startActivity(intent);
-    }
+        btn_login=findViewById(R.id.login_signin_btn);
+        btn_signup= findViewById(R.id.login_signup_btn);
+        username=findViewById(R.id.login_username);
+        password=findViewById(R.id.login_password);
+        db=FirebaseFirestore.getInstance();
+       btn_login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               DocumentReference docRef = db.collection("students").document(username.getText().toString());
+               docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                   @Override
+                   public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                       if (task.isSuccessful()) {
+                           DocumentSnapshot document = task.getResult();
+                           if (document.exists()) {
+                               Map<String,Object> student=document.getData();
+                               if (student.get("password").toString().equals(password.getText().toString()))
+                               {
+//                                   SharedPreferences sp=getSharedPreferences("myquiz",MODE_PRIVATE);
+//                                   SharedPreferences.Editor editor=sp.edit();
+//                                   editor.putString("userid",username.getText().toString());
+//                                   editor.commit();
+                                   Intent intent=new Intent(login_page.this,MainScreen.class);
+                                   startActivity(intent);
+                               }
 
-    public void  openActivity2(){
-        Intent intent = new Intent(this,Signup.class);
-        startActivity(intent);
+                           } else {
+
+                               Toast.makeText(login_page.this, "invalid user", Toast.LENGTH_SHORT).show();
+                           }
+                       } else {
+                           Toast.makeText(login_page.this, "connectivity issue", Toast.LENGTH_SHORT).show();
+                       }
+                   }
+               });
+           }
+       });
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(login_page.this,Signup.class);
+                startActivity(intent);
+            }
+        });
     }
 
     }
